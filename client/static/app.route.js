da71bd5f0ab7315717d671c9796091c893e77578
francoa <francoamg@gmail.com>
2016-10-25 22:03:11 -0300

    static en app.route

--- client/static/app.route.js
+++ client/static/app.route.js
@@ -7,17 +7,17 @@
       function($routeProvider) {
         $routeProvider
           .when('/login', {
-            templateUrl: 'beer/templates/login.html',
+            templateUrl: 'static/beer/templates/login.html',
             controller: 'LoginController',
             controllerAs: 'loginCtrl'
           })
           .when('/monitor',{
-            templateUrl: 'beer/templates/monitor.html',
+            templateUrl: 'static/beer/templates/monitor.html',
             controller: 'MonitorController',
             controllerAs: 'monitorCtrl'
           })
           .when('/admin',{
-            templateUrl: 'beer/templates/admin.html',
+            templateUrl: 'static/beer/templates/admin.html',
             controller: 'AdminController',
             controllerAs: 'adminCtrl'
           })

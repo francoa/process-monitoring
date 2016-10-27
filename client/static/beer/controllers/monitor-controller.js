44d0842183feefa4246008aa6f4a036f4eae3e65
francoa <francoamg@gmail.com>
2016-10-26 21:18:43 -0300

    Task bar

--- client/static/beer/controllers/monitor-controller.js
+++ client/static/beer/controllers/monitor-controller.js
@@ -7,6 +7,24 @@
 
       //OBVIOUS TODO: GET RECIPES FROM SERVER
       $scope.recetas = ["Receta 1", "Receta 2", "Receta 3"];
+      $scope.paso = 0;
+      $scope.instrucciones = [
+        {"msg":"Calentar agua","autom":false},
+        {"msg":"Colocar bombas para trasvase de agua","autom":false},
+        {"msg":"Alcanzar TºC de olla licor","autom":false},
+        {"msg":"Alcanzar TºC para macerar","autom":false},
+        {"msg":"Mash-in","autom":false},
+        {"msg":"Control tiempos","autom":false},
+        {"msg":"Colocar bomba en herms","autom":false},
+        {"msg":"Colocar mangueras herms","autom":false},
+        {"msg":"Segundo escalon de TºC","autom":true},
+        {"msg":"Medir tiempo","autom":false},
+        {"msg":"Mash-out","autom":false},
+        {"msg":"Reposicionar bombas recirculado","autom":false},
+        {"msg":"Recirculado","autom":false},
+        {"msg":"Colocar bombas para knock-out","autom":false},
+        {"msg":"Colocar bombas para lavado","autom":false},
+        {"msg":"Lavado de grano","autom":false}];
 
       $scope.startProcess=function(receta){
         $window.alert(receta);
@@ -19,6 +37,10 @@
 
       $scope.cambiarContrasena = function(user){
         $window.alert("Cambio de contraseña");
+      }
+
+      $scope.pasoSiguiente = function(){
+        $scope.paso = $scope.paso+1;
       }
 
       $scope.popupModal=function(name){
